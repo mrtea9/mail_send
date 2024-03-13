e7cc0676567609710fff3537cf7cc6257d69fbbd
mrtea9 <ionmeriacri111@gmail.com>
2024-03-13 15:26:16 +0200

    Send Mail

--- main.py
+++ main.py
@@ -1,11 +1,3 @@
-# Create a letter using starting_letter.txt
-# for each name in invited_names.txt
-# Replace the [name] placeholder with the actual name.
-# Save the letters in the folder "ReadyToSend".
-
-# Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
-# Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
-# Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
 
 letter_location = '.\\Input\\Letters\\starting_letter.txt'
 names_location = '.\\Input\\\\Names\\invited_names.txt'
